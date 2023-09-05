0e4ad3360a55242828bf02bc913e2aabf0f3fc83
salbyul <kpast96@gmail.com>
2023-09-05 17:56:26 +0900

    init

---
+++ src/main/java/com/coro/coro/CoroApplication.java
@@ -0,0 +1,13 @@
+package com.coro.coro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coro/coro/CoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coro.coro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
