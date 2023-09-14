15f867705159370c4b583e2116bb3e29ec5a613a
salbyul <kpast96@gmail.com>
2023-09-14 18:03:52 +0900

    docs: spring docs 수정

--- src/main/java/com/coro/coro/moim/controller/MoimControllerDocs.java
+++ src/main/java/com/coro/coro/moim/controller/MoimControllerDocs.java
@@ -2,6 +2,7 @@ package com.coro.coro.moim.controller;
 
 import com.coro.coro.common.response.APIResponse;
 import com.coro.coro.member.domain.User;
+import com.coro.coro.moim.dto.request.MoimModifyRequest;
 import com.coro.coro.moim.dto.request.MoimRegisterRequest;
 import com.coro.coro.moim.dto.request.MoimTagRequest;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +13,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Tag(name = "Moim", description = "모임")
 public interface MoimControllerDocs {
@@ -19,14 +23,17 @@ public interface MoimControllerDocs {
     @Operation(summary = "모임 등록")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "모임 등록 성공"),
-            @ApiResponse(responseCode = "400", description = "모임 등록 실패")
+            @ApiResponse(responseCode = "400", description = "모임 등록 실패"),
+            @ApiResponse(responseCode = "403", description = "유저 인증 실패")
     })
     @Parameters(value = {
-            @Parameter(name = "name", description = "모임명", example = "공부모임"),
+            @Parameter(name = "name", description = "모임명", example = "공부모임", required = true),
             @Parameter(name = "introduction", description = "모임 소개", example = "우리 모임을 소개합니다!"),
             @Parameter(name = "type", description = "모임 대면, 비대면 타입", example = "mixed faceToFace nonContact"),
-            @Parameter(name = "visible", description = "공개 모임인지", example = "true"),
+            @Parameter(name = "visible", description = "공개 모임인지", example = "true", required = true),
             @Parameter(name = "tagList", description = "모임 태그들", example = "[\"tag1\", \"tag2\", \"tag3\"]")
     })
     APIResponse register(@RequestPart final MoimRegisterRequest requestMoim, @RequestPart final MoimTagRequest requestTag, @AuthenticationPrincipal final User user);
+
+    APIResponse update(@RequestPart final MoimModifyRequest requestMoim, @RequestPart final MoimTagRequest requestTag, @RequestPart List<MultipartFile> multipartFileList, @AuthenticationPrincipal final User user);
 }
