243854c3cf666aaeb7e6b738a1fbe7dcd864c445
salbyul <kpast96@gmail.com>
2023-10-19 12:52:59 +0900

    docs:

--- src/main/java/com/coro/coro/moim/controller/MoimController.java
+++ src/main/java/com/coro/coro/moim/controller/MoimController.java
@@ -33,6 +33,13 @@ public class MoimController implements MoimControllerDocs {
 
     private final MoimService moimService;
 
+    /**
+     * 모임의 디테일 정보 획득
+     * @param moimId 디테일 정보를 획득할 모임의 Id 값
+     * @param user 로그인한 유저
+     * @return 모임의 디테일 정보
+     * @throws IOException 이미지 파일로 인한 예외
+     */
     @GetMapping("/{id}")
     @Override
     public APIResponse detail(@PathVariable("id") final Long moimId, @AuthenticationPrincipal final User user) throws IOException {
@@ -41,6 +48,14 @@ public class MoimController implements MoimControllerDocs {
                 .addObject("moim", detail);
     }
 
+    /**
+     * 모임 검색
+     * 모임의 이름 혹은 태그로 검색 가능
+     * @param moimSearchRequest 검색 조건 DTO
+     * @param pageable 페이징
+     * @return 검색 결과
+     * @throws IOException 이미지 파일로 인한 예외
+     */
     @GetMapping("/search")
     @Override
     public APIResponse search(@Search final MoimSearchRequest moimSearchRequest, final Pageable pageable) throws IOException {
@@ -56,6 +71,16 @@ public class MoimController implements MoimControllerDocs {
                 .addObject("isLast", result.isLast());
     }
 
+    /**
+     * 모임 생성
+     * @param requestMoim 생성할 모임의 데이터가 담긴 객체
+     * @param requestTag 생성할 모임의 태그리스트가 담긴 객체
+     * @param requestQuestions 생성할 모임의 지원 양식이 담긴 객체
+     * @param multipartFile 생성할 모임의 이미지 파일
+     * @param user 로그인한 유저
+     * @return 생성된 모임의 Id 값
+     * @throws IOException 이미지 파일로 인한 예외
+     */
     @PostMapping
     @Override
     public APIResponse register(@RequestPart(name = "moim") final MoimRegisterRequest requestMoim,
@@ -68,6 +93,17 @@ public class MoimController implements MoimControllerDocs {
                 .addObject("moimId", savedId);
     }
 
+    /**
+     * 모임 수정
+     * @param moimId 수정할 모임의 Id 값
+     * @param requestMoim 수정할 모임 데이터가 담긴 객체
+     * @param requestTag 수정된 태그 리스트가 담긴 객체
+     * @param requestQuestions 수정된 지원 양식이 담긴 객체
+     * @param multipartFile 수정된 이미지 파일
+     * @param user 로그인한 유저
+     * @return 반환값 없음
+     * @throws IOException 이미지 파일로 인한 예외
+     */
     @PutMapping("/{id}")
     @Override
     public APIResponse update(@PathVariable("id") Long moimId,
@@ -80,6 +116,13 @@ public class MoimController implements MoimControllerDocs {
         return APIResponse.create();
     }
 
+    /**
+     * 모임 수정을 위한 모임의 데이터 획득
+     * @param moimId 해당 모임의 Id 값
+     * @param user 로그인한 유저
+     * @return 모임의 데이터가 담긴 객체
+     * @throws IOException 이미지 파일로 인한 예외
+     */
     @GetMapping("/modification/{id}")
     @Override
     public APIResponse getMoimForModification(@PathVariable("id") final Long moimId, @AuthenticationPrincipal User user) throws IOException {
@@ -88,6 +131,12 @@ public class MoimController implements MoimControllerDocs {
                 .addObject("detail", detail);
     }
 
+    /**
+     * 해당 모임에 가입된 회원 리스트 획득
+     * @param moimId 해당 모임의 Id 값
+     * @param user 로그인한 유저
+     * @return 모임에 가입된 회원 리스트
+     */
     @GetMapping("/{moimId}/members")
     @Override
     public APIResponse getMoimMember(@PathVariable("moimId") final Long moimId, @AuthenticationPrincipal final User user) {
@@ -98,6 +147,13 @@ public class MoimController implements MoimControllerDocs {
                 .addObject("role", loggedInMemberRole);
     }
 
+    /**
+     * 모임에 가입된 회원들의 역할 변경
+     * @param moimId 해당 모임의 Id 값
+     * @param requestMoimMember 수정된 회원들의 정보가 담긴 객체
+     * @param user 로그인한 유저
+     * @return 반환값 없음
+     */
     @PutMapping("/{moimId}/members")
     @Override
     public APIResponse changeMoimMember(@PathVariable("moimId") final Long moimId,
@@ -107,6 +163,13 @@ public class MoimController implements MoimControllerDocs {
         return APIResponse.create();
     }
 
+    /**
+     * 회원 추방
+     * @param moimId 해당 모임의 Id 값
+     * @param moimMemberId 추방될 회원의 MoimMember Id 값
+     * @param user 로그인한 유저
+     * @return 반환값 없음
+     */
     @DeleteMapping("/{moimId}/members")
     @Override
     public APIResponse deportMember(@PathVariable(name = "moimId") final Long moimId, @ModelAttribute(name = "moimMember") final Long moimMemberId, @AuthenticationPrincipal final User user) {
